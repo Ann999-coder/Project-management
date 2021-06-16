2b51ea5a9b5459934578e626d8678b0e56b7ad76
Ann999-coder <67745510+Ann999-coder@users.noreply.github.com>
2021-06-17 00:46:06 +0530

    Update projectController.java

--- Projectmanagemet/src/main/java/com/nosce/pkg/controller/projectController.java
+++ Projectmanagemet/src/main/java/com/nosce/pkg/controller/projectController.java
@@ -1,8 +1,11 @@
 package com.nosce.pkg.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.nosce.pkg.model.Project;
 import com.nosce.pkg.service.impl.IprojectService;
+
 
 
 @RestController
@@ -39,5 +43,8 @@ public class projectController {
 		return projectservice.list();
 		
 	}
+	
+	
+
 
 }
