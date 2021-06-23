2ceedaff26e36f33f1ff9dd20b136aee97560f9f
Ann999-coder <67745510+Ann999-coder@users.noreply.github.com>
2021-06-23 21:38:58 +0530

    Update IteamService.java

--- Projectmanagemet/src/main/java/com/nosce/pkg/service/impl/IteamService.java
+++ Projectmanagemet/src/main/java/com/nosce/pkg/service/impl/IteamService.java
@@ -1,9 +1,11 @@
 package com.nosce.pkg.service.impl;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 import com.nosce.pkg.model.Team;
 import com.nosce.pkg.repository.teamRepository;
@@ -28,6 +30,26 @@ public class IteamService implements teamService {
 	public List<Team> list() {
 		// TODO Auto-generated method stub
 		return teamrepository.findAll();
+	}
+
+	@Override
+	public void update(Team team, int teamid) {
+		// TODO Auto-generated method stub
+		teamrepository.save(team);
+		
+	}
+
+	@Override
+	public Optional<Team> getById(Long teamid) {
+		// TODO Auto-generated method stub
+		return teamrepository.findById(teamid);
+	}
+
+	@Override
+	public void delete(Long teamid) {
+		// TODO Auto-generated method stub
+		teamrepository.deleteById(teamid);
+		
 	}
 
 }
