be4f55a790352cc533d29463feb5741ffc160e6d
Ann999-coder <67745510+Ann999-coder@users.noreply.github.com>
2021-06-23 21:36:23 +0530

    Update projectService.java

--- Projectmanagemet/src/main/java/com/nosce/pkg/service/projectService.java
+++ Projectmanagemet/src/main/java/com/nosce/pkg/service/projectService.java
@@ -1,12 +1,19 @@
 package com.nosce.pkg.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.nosce.pkg.model.Project;
+
 
 public interface projectService {
 
 	public void add(Project project);
+	public void update(Project project,int projectid);
+	Optional<Project> getById(Long projectid);
+	
+	public void delete(Long projectid);
+	 
 	
 	List<Project> list();
 }
