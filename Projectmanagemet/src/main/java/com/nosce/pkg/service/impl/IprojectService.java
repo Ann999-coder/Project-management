07fb7d1c25245746737fbd3df2736ea8216e9dc7
Ann999-coder <67745510+Ann999-coder@users.noreply.github.com>
2021-06-23 21:38:24 +0530

    Update IprojectService.java

--- Projectmanagemet/src/main/java/com/nosce/pkg/service/impl/IprojectService.java
+++ Projectmanagemet/src/main/java/com/nosce/pkg/service/impl/IprojectService.java
@@ -1,5 +1,6 @@
 package com.nosce.pkg.service.impl;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,4 +30,34 @@ public class IprojectService implements projectService {
 		return projectrepository.findAll();
 	}
 
+	@Override
+	public void update(Project project, int projectid) {
+		// TODO Auto-generated method stub
+		projectrepository.save(project);
+		
+	}
+
+	
+
+
+	@Override
+	public Optional<Project> getById(Long projectid) {
+		// TODO Auto-generated method stub
+		return  projectrepository.findById(projectid);
+	}
+
+	@Override
+	public void delete(Long projectid) {
+		// TODO Auto-generated method stub
+		
+		projectrepository.deleteById(projectid);
+	}
+
+	
+	
+
+	
+
+
+	
 }
