d2bb585e0ac53df9868d87ee635276b3e4d000b6
Ann999-coder <67745510+Ann999-coder@users.noreply.github.com>
2021-06-23 21:36:59 +0530

    Update clientService.java

--- Projectmanagemet/src/main/java/com/nosce/pkg/service/clientService.java
+++ Projectmanagemet/src/main/java/com/nosce/pkg/service/clientService.java
@@ -1,13 +1,20 @@
 package com.nosce.pkg.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.nosce.pkg.model.Client;
+
 
 
 public interface clientService {
 
 	public void add(Client client);
+	public void update(Client client,int clientid);
+	Optional<Client> getById(Long clientid);
+	
+	public void delete(Long clientid);
+	
 	
 	List<Client> list();
 }
