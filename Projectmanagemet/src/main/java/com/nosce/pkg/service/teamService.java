b90994eb837b7939f0180889b21d263652f5e9d9
Ann999-coder <67745510+Ann999-coder@users.noreply.github.com>
2021-06-23 21:35:41 +0530

    Update teamService.java

--- Projectmanagemet/src/main/java/com/nosce/pkg/service/teamService.java
+++ Projectmanagemet/src/main/java/com/nosce/pkg/service/teamService.java
@@ -1,12 +1,18 @@
 package com.nosce.pkg.service;
 
 import java.util.List;
+import java.util.Optional;
+
 
 import com.nosce.pkg.model.Team;
 
 public interface teamService {
 
 	public void add(Team team);
+	public void update(Team team,int teamid);
+	Optional<Team> getById(Long teamid);
+	
+	public void delete(Long teamid);
 	
 	List<Team> list();
 }
