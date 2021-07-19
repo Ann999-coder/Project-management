4ec5475b0c77b951774b07d7cdced0d6d8a791b4
Ann999-coder <67745510+Ann999-coder@users.noreply.github.com>
2021-07-19 19:37:29 +0530

    Update Client.java

--- Projectmanagemet/src/main/java/com/nosce/pkg/model/Client.java
+++ Projectmanagemet/src/main/java/com/nosce/pkg/model/Client.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name="TBL_CLIENT")
@@ -16,37 +17,29 @@ public class Client {
     private Long id;
      
     @Column(name="Company")
+    @NotBlank(message = "company is mandatory")
     private String company;
      
     @Column(name="Website")
+    @NotBlank(message = "website is mandatory")
     private String website;
     
     @Column(name="Contact")
+    @NotBlank(message = "contact is mandatory")
     private String contact;
     
+    @Column(name="email", nullable=false, length=200)
+    @NotBlank(message = "message is mandatory")
+    private String email;
+    
     @Column(name="UserName")
+    @NotBlank(message = "userName is mandatory")
     private String userName;
     
     @Column(name="Password")
+    @NotBlank(message = "password is mandatory")
     private String password;
 
-     
-    public Client() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public Client(Long id, String company, String website, String contact, String userName, String password) {
-		super();
-		this.id = id;
-		this.company = company;
-		this.website = website;
-		this.contact = contact;
-		this.userName = userName;
-		this.password = password;
-	}
-
-	
 	public Long getId() {
 		return id;
 	}
@@ -79,6 +72,14 @@ public class Client {
 		this.contact = contact;
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public String getUserName() {
 		return userName;
 	}
@@ -95,6 +96,23 @@ public class Client {
 		this.password = password;
 	}
 
+	public Client(Long id, String company, String website, String contact, String email, String userName,
+			String password) {
+		super();
+		this.id = id;
+		this.company = company;
+		this.website = website;
+		this.contact = contact;
+		this.email = email;
+		this.userName = userName;
+		this.password = password;
+	}
 
+	public Client() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
+     
+    
 }
